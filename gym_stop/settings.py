b7c0c1792a02d9f0c4322b1126dae6dfc55b6e11
Dan Hodgson <danhodgson12@gmail.com>
2024-04-28 08:54:58 +0000

    feat: completed confirmation emails

--- gym_stop/settings.py
+++ gym_stop/settings.py
@@ -183,11 +183,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Stripe
-
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+
+DEFAULT_FROM_EMAIL = 'gym-stop@example.com'
