30997eca01d39c964f32f0f99c99209d353fe181
Dan Hodgson <danhodgson12@gmail.com>
2024-05-06 13:57:34 +0000

    fix: linting issues and dead space

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -2,10 +2,9 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-
 from checkout.webhook_handler import StripeWH_Handler
-
 import stripe
+
 
 @require_POST
 @csrf_exempt
@@ -52,4 +51,3 @@ def webhook(request):
     # Call the event handler with the event
     response = event_handler(event)
     return response
-
