70f0a760c63e6dc945df127436745612208cbbd4
Dan Hodgson <danhodgson12@gmail.com>
2024-05-10 21:19:16 +0000

    feat: added view to create instance of marketing subscription form

--- home/views.py
+++ home/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from .forms import MarketingSubscriptionForm
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    form = MarketingSubscriptionForm()
+
+    return render(request, 'home/index.html', {'form': form})
+
+
+def subscribe(request):
+    if request.method == 'POST':
+        form = MarketingSubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Optionally, generate and send a discount code to the subscriber
+            messages.success(request, f'Thanks for subscribing! A welcome email and your discount code will be sent to {form.cleaned_data["email"]}.')
+            return redirect('home')
+    else:
+        form = MarketingSubscriptionForm()
+        messages.error(request, 'An error occurred when submitting your form. Please try again.')
+    return render(request, 'home/index.html', {'form': form})
