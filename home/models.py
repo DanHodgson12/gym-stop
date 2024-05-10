09e63809aeedaf9cb6d800201e2707b70be2fc05
Dan Hodgson <danhodgson12@gmail.com>
2024-05-10 21:17:53 +0000

    feat: added marketing subscription model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class MarketingSubscription(models.Model):
+    email = models.EmailField(unique=True)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+    discount_code = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.email
