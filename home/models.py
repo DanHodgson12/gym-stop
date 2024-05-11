3ce88bbc68ca5670c6975d656f538546343cf412
Dan Hodgson <danhodgson12@gmail.com>
2024-05-11 14:33:24 +0000

    feat: adjusted subscription model

--- home/models.py
+++ home/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class MarketingSubscription(models.Model):
     email = models.EmailField(unique=True)
     date_subscribed = models.DateTimeField(auto_now_add=True)
-    discount_code = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.email
