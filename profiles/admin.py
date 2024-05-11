593020ee7961e43c923e385c1474b7d413314f6a
Dan Hodgson <danhodgson12@gmail.com>
2024-05-11 19:49:52 +0000

    feat: created userprofile section for admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ('user', 'default_phone_number',
+            'default_street_address1', 'default_street_address2',
+            'default_town_or_city', 'default_county',
+            'default_country', 'default_postcode',
+            'is_subscribed_to_newsletter')
+
+    list_display = ('user', 'default_phone_number',
+                    'is_subscribed_to_newsletter')
+
+    ordering = ('-user',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
