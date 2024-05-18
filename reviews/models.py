e5ef506fcfab760cc62af9dbaf9b83bf8effedea
Dan Hodgson <danhodgson12@gmail.com>
2024-05-18 15:36:59 +0000

    feat: added headline field and altered content field for review model

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,8 @@ from products.models import Product
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    content = models.TextField()
+    headline = models.CharField(max_length=50, blank=True)
+    content = models.TextField(max_length=500, blank=True)
     rating = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
