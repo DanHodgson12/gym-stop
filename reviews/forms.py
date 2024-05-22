7b3ae88c1c3c4557a17e8f046bd28a8f792844fe
Dan Hodgson <danhodgson12@gmail.com>
2024-05-22 14:57:53 +0000

    feat: changed rating integer field into radio select box and added styling

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,17 +1,32 @@
 from django import forms
+from django.utils.safestring import mark_safe
 from .models import Review
 
 class ReviewForm(forms.ModelForm):
+
     def __init__(self, *args, **kwargs):
         super(ReviewForm, self).__init__(*args, **kwargs)
+        
+        # Star Rating Choices
+        rating_choices = [
+            (i, mark_safe(f"{i} <i class='fa fa-star filled-star'></i>"))
+            for i in range(1, 6)
+        ]
+
+        self.fields['rating'] = forms.ChoiceField(
+            choices=rating_choices,
+            widget=forms.RadioSelect(attrs={'class': 'filled-star'}),
+            label="Rating"
+        )
+
         placeholders = {
             'headline': 'Brief headline for your review',
             'content': 'Write your review here'
         }
-        
+
         for field_name, placeholder in placeholders.items():
             if field_name in self.fields:
-                self.fields[field_name].widget.attrs.update({'placeholder': placeholder})
+                self.fields[field_name].widget.attrs.update({'placeholder': placeholder})    
 
     class Meta:
         model = Review
