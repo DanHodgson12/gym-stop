24e1fe5b4931e49d481c93045f8e05b4613cfd3d
Dan Hodgson <danhodgson12@gmail.com>
2024-05-22 16:26:35 +0000

    feat: registered review admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'rating',
+        'user',
+        'headline',
+        'created_at',
+    )
+
+    ordering = ('created_at',)
+
+
+admin.site.register(Review, ReviewAdmin)
