a1083ab53aa4e8a5f185f8825eb3dc54949eac37
Dan Hodgson <danhodgson12@gmail.com>
2024-05-22 14:05:05 +0000

    feat: added value validators to rating field of review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 from products.models import Product
 
@@ -7,7 +8,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     headline = models.CharField(max_length=50, blank=True)
     content = models.TextField(max_length=500, blank=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
