1e57784ba7711a082004d8b9ce60c7464597ce29
Dan Hodgson <danhodgson12@gmail.com>
2024-05-23 10:54:02 +0000

    feat: added login_required decorator to add_review view

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
 from .forms import ReviewForm
 from products.models import Product
 
 
+@login_required
 def add_review(request, product_id):
-    product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product, id=product_id)
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
             review.product = product
-            review.user = request.user if request.user.is_authenticated else None
+            review.user = request.user
             review.save()
             return redirect('product_detail', product_id=product_id)
     else:
