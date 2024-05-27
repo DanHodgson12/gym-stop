2e5e9ae3c9e3baa32f5994892148eeefad35183c
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 18:19:39 +0000

    feat: added and reformatted docstrings for test cases

--- products/test_widgets.py
+++ products/test_widgets.py
@@ -7,8 +7,15 @@ from .forms import ProductForm
 from .models import Category, Product
 
 class CustomClearableFileInputTests(TestCase):
+    """
+    Test cases for the CustomClearableFileInput widget to ensure it behaves correctly.
+    """
 
     def setUp(self):
+        """
+        Set up test data for CustomClearableFileInput widget tests.
+        """
+
         self.category = Category.objects.create(name='Test Category', friendly_name='Test Category')
         self.product = Product.objects.create(
             sku='12345',
@@ -20,13 +27,19 @@ class CustomClearableFileInputTests(TestCase):
         )
 
     def test_widget_template(self):
-        """Test that the CustomClearableFileInput widget uses the correct template."""
+        """
+        Test that the CustomClearableFileInput widget uses the correct template.
+        """
+
         form = ProductForm(instance=self.product)
         widget = form.fields['image'].widget
         self.assertEqual(widget.template_name, 'products/custom_widget_templates/custom_clearable_file_input.html')
 
     def test_widget_labels(self):
-        """Test that the CustomClearableFileInput widget has the correct labels."""
+        """
+        Test that the CustomClearableFileInput widget has the correct labels.
+        """
+
         form = ProductForm(instance=self.product)
         widget = form.fields['image'].widget
         self.assertEqual(widget.clear_checkbox_label, 'Remove')
@@ -34,7 +47,10 @@ class CustomClearableFileInputTests(TestCase):
         self.assertEqual(widget.input_text, '')
 
     def test_widget_render(self):
-        """Test that the CustomClearableFileInput widget renders correctly in a form."""
+        """
+        Test that the CustomClearableFileInput widget renders correctly in a form.
+        """
+
         form = ProductForm(instance=self.product)
         template = Template('{{ form.image }}')
         context = Context({'form': form})
