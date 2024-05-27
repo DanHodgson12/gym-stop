2754c3361a7784d33b4d5fcabbfeb9fcd7fdc440
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 17:50:12 +0000

    feat: reformatted docstrings for test functions

--- bag/test_views.py
+++ bag/test_views.py
@@ -21,13 +21,19 @@ class BagViewsTests(TestCase):
         self.remove_from_bag_url = reverse('remove_from_bag', args=[self.product.id])
 
     def test_view_bag(self):
-        """Test viewing the bag page."""
+        """
+        Test viewing the bag page.
+        """
+
         response = self.client.get(self.bag_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bag/bag.html')
 
     def test_add_to_bag(self):
-        """Test adding a product to the bag."""
+        """
+        Test adding a product to the bag.
+        """
+
         response = self.client.post(self.add_to_bag_url, {
             'quantity': 1,
             'redirect_url': self.bag_url
@@ -38,7 +44,10 @@ class BagViewsTests(TestCase):
         self.assertEqual(session['bag'][str(self.product.id)], 1)
 
     def test_add_to_bag_with_size(self):
-        """Test adding a product with size to the bag."""
+        """
+        Test adding a product with size to the bag.
+        """
+
         response = self.client.post(self.add_to_bag_url, {
             'quantity': 1,
             'product_size': 'M',
@@ -51,7 +60,10 @@ class BagViewsTests(TestCase):
         self.assertEqual(session['bag'][str(self.product.id)]['items_by_size']['M'], 1)
 
     def test_adjust_bag(self):
-        """Test adjusting the quantity of a product in the bag."""
+        """
+        Test adjusting the quantity of a product in the bag.
+        """
+
         session = self.client.session
         session['bag'] = {str(self.product.id): 1}
         session.save()
@@ -61,7 +73,10 @@ class BagViewsTests(TestCase):
         self.assertEqual(session['bag'][str(self.product.id)], 2)
 
     def test_adjust_bag_with_size(self):
-        """Test adjusting the quantity of a product with size in the bag."""
+        """
+        Test adjusting the quantity of a product with size in the bag.
+        """
+
         session = self.client.session
         session['bag'] = {str(self.product.id): {'items_by_size': {'M': 1}}}
         session.save()
@@ -71,7 +86,10 @@ class BagViewsTests(TestCase):
         self.assertEqual(session['bag'][str(self.product.id)]['items_by_size']['M'], 2)
 
     def test_remove_from_bag(self):
-        """Test removing a product from the bag."""
+        """
+        Test removing a product from the bag.
+        """
+
         session = self.client.session
         session['bag'] = {str(self.product.id): 1}
         session.save()
@@ -81,7 +99,10 @@ class BagViewsTests(TestCase):
         self.assertNotIn(str(self.product.id), session['bag'])
 
     def test_remove_from_bag_with_size(self):
-        """Test removing a product with size from the bag."""
+        """
+        Test removing a product with size from the bag.
+        """
+
         session = self.client.session
         session['bag'] = {str(self.product.id): {'items_by_size': {'M': 1}}}
         session.save()
