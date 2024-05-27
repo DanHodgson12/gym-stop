1c58ccfa8de4a2a7633c485ab39b4015e8e1ad2d
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 18:25:01 +0000

    feat: added and reformatted docstrings for test cases

--- profiles/test_forms.py
+++ profiles/test_forms.py
@@ -5,8 +5,15 @@ from django_countries.fields import Country
 
 
 class UserProfileFormTests(TestCase):
+    """
+    Test cases for the UserProfileForm to ensure it behaves correctly.
+    """
 
     def setUp(self):
+        """
+        Set up the initial data for the UserProfileForm tests.
+        """
+
         self.form_data = {
             'default_phone_number': '123456789',
             'default_street_address1': 'Test Street 1',
@@ -19,17 +26,27 @@ class UserProfileFormTests(TestCase):
         }
 
     def test_user_profile_form_valid_data(self):
-        """Test that the UserProfileForm is valid with valid data."""
+        """
+        Test that the UserProfileForm is valid with valid data.
+        """
+
         form = UserProfileForm(data=self.form_data)
         self.assertTrue(form.is_valid())
 
     def test_user_profile_form_empty_data(self):
-        """Test that the UserProfileForm is valid with empty data since no fields are required."""
+        """
+        Test that the UserProfileForm is valid with empty data
+        since no fields are required.
+        """
+
         form = UserProfileForm(data={})
         self.assertTrue(form.is_valid())
 
     def test_user_profile_form_placeholders(self):
-        """Test that the UserProfileForm has correct placeholders and classes."""
+        """
+        Test that the UserProfileForm has correct placeholders and classes.
+        """
+
         form = UserProfileForm()
         placeholders = {
             'default_phone_number': 'Phone Number',
@@ -50,6 +67,10 @@ class UserProfileFormTests(TestCase):
                 self.assertEqual(form.fields[field].widget.attrs['class'], 'border-black')
 
     def test_user_profile_form_autofocus(self):
-        """Test that the UserProfileForm sets autofocus on the phone number field."""
+        """
+        Test that the UserProfileForm sets autofocus
+        on the phone number field.
+        """
+
         form = UserProfileForm()
         self.assertTrue(form.fields['default_phone_number'].widget.attrs['autofocus'])
