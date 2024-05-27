af248a9c24596edec325c106fccb3c526808187e
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 18:37:37 +0000

    feat: added docstring

--- subscribe/contexts.py
+++ subscribe/contexts.py
@@ -2,6 +2,10 @@ from .forms import MarketingSubscriptionForm
 
 
 def subscribe_form(request):
+    """
+    Context processor to add the marketing subscription form to the context.
+    """
+
     return {
         'subscribe_form': MarketingSubscriptionForm()
     }
