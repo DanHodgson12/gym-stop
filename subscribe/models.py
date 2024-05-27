7c013ff1d127b4745e2840807afbbc6dbd84353c
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 18:38:52 +0000

    feat: added docstrings

--- subscribe/models.py
+++ subscribe/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class MarketingSubscription(models.Model):
+    """
+    Model representing a subscription to marketing emails.
+    """
+
     email = models.EmailField(unique=True)
     date_subscribed = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        String representation of the MarketingSubscription.
+        """
+
         return self.email
