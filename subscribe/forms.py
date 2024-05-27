184d79a4678f49b7dc00ef4f026b790182f14a14
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 18:38:13 +0000

    feat: added docstrings

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -3,10 +3,18 @@ from .models import MarketingSubscription
 
 
 class MarketingSubscriptionForm(forms.ModelForm):
+    """
+    Form for subscribing to marketing emails.
+    """
+
     class Meta:
         model = MarketingSubscription
         fields = ['email']
 
 
 class UnsubscribeForm(forms.Form):
+    """
+    Form for unsubscribing from marketing emails.
+    """
+
     email = forms.EmailField(label='Email')
