11091da253fbe98361a9dc17303c181bcc3e096b
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 17:50:56 +0000

    feat: reformatted docstrings for views

--- bag/views.py
+++ bag/views.py
@@ -5,13 +5,17 @@ from products.models import Product
 
 
 def view_bag(request):
-    """ A view that renders the bag contents page. """
+    """
+    A view that renders the bag contents page.
+    """
 
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag. """
+    """
+    Add a quantity of the specified product to the shopping bag.
+    """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -112,7 +116,9 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Remove the item from the shopping bag. """
+    """
+    Remove the item from the shopping bag.
+    """
 
     try:
         product = get_object_or_404(Product, pk=item_id)
