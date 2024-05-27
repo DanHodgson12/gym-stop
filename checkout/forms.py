ffe1f32e5afe92bdc9792a43dffacadf4182cfc6
Dan Hodgson <danhodgson12@gmail.com>
2024-05-27 17:56:21 +0000

    feat: added docstring

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,6 +3,10 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    """
+    Form for creating and updating Order instances.
+    """
+
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
